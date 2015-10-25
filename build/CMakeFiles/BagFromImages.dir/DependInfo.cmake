e4f2b9eb624989cc55507915ad5aba97013c43de
Boris Ivanovic <boris.ivanovic@mail.utoronto.ca>
2015-10-24 22:44:47 -0400

    Built BagFromImages.

---
+++ build/CMakeFiles/BagFromImages.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bivanovi/rosbuild_ws/BagFromImages/Thirdparty/DLib/FileFunctions.cpp" "/home/bivanovi/rosbuild_ws/BagFromImages/build/CMakeFiles/BagFromImages.dir/Thirdparty/DLib/FileFunctions.cpp.o"
+  "/home/bivanovi/rosbuild_ws/BagFromImages/main.cc" "/home/bivanovi/rosbuild_ws/BagFromImages/build/CMakeFiles/BagFromImages.dir/main.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ROS_PACKAGE_NAME=\"BagFromImages\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/indigo/include"
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/lib:/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/lib/pkgconfig:/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/bivanovi/rosbuild_ws/BagFromImages/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/bivanovi/rosbuild_ws/BagFromImages:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
